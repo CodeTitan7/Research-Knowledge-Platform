1ac7ebeafdc91835d4653f5d1407ee5a17fdd027
V Devanarayan <89205573+CodeTitan7@users.noreply.github.com>
2026-08-12 10:32:46 +0530

    Change minimum length of Question property to 2

--- CompoundResearchAPI/Models/DTOs/QueryRequestDto.cs
+++ CompoundResearchAPI/Models/DTOs/QueryRequestDto.cs
@@ -4,7 +4,7 @@ namespace CompoundResearchAPI.Models.DTOs
 {
     public class QueryRequestDto
     {
-        [Required, StringLength(1000, MinimumLength = 3)]
+        [Required, StringLength(1000, MinimumLength = 2)]
         public string Question { get; set; } = string.Empty;
 
         // Optional: narrow the search to documents linked to a specific compound.
